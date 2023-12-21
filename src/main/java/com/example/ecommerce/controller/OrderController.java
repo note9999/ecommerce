89ea60b2f30ff83a7b60243989e6d822009d6307
note9999 <sanghoon273@gmail.com>
2023-12-21 15:50:31 +0900

    컨트롤러 import 에러 해결

--- src/main/java/com/example/ecommerce/controller/OrderController.java
+++ src/main/java/com/example/ecommerce/controller/OrderController.java
@@ -1,4 +1,22 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.exception.InsufficientBalanceException;
+import com.example.ecommerce.exception.InsufficientStockException;
+import com.example.ecommerce.model.Order;
+import com.example.ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/place")
+    public void placeOrder(@RequestBody Order order) throws InsufficientStockException, InsufficientBalanceException {
+        orderService.placeOrder(order);
+    }
 }
+
--- src/main/java/com/example/ecommerce/controller/TopProductsController.java
+++ src/main/java/com/example/ecommerce/controller/TopProductsController.java
@@ -1,4 +1,23 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.model.Product;
+import com.example.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/top-products")
 public class TopProductsController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getTopProducts() {
+        return productService.getTopProducts();
+    }
 }
