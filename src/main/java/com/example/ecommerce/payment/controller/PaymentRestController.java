a99d96896fc3e480c402169c087cd4c50da81205
note9999 <sanghoon273@gmail.com>
2023-12-28 17:52:37 +0900

    PaymentRestController

--- src/main/java/com/example/ecommerce/payment/controller/PaymentRestController.java
+++ src/main/java/com/example/ecommerce/payment/controller/PaymentRestController.java
@@ -1,4 +1,18 @@
 package com.example.ecommerce.payment.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/payments")
 public class PaymentRestController {
+
+    @PostMapping("/request")
+    public ResponseEntity<?> requestPayments() {
+        return ResponseEntity.ok("주문 결제");
+    }
 }
